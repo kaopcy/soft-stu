68be617715ce0ff1c48274b39f074177dcd0b55e
piyachai kaewchum <kaonaka@hotmail.co.th>
2024-02-10 17:13:09 +0700

    fix date/time to continue the actual time

--- activity9/worker.js
+++ activity9/worker.js
@@ -2,7 +2,10 @@ const colors = ["red", "blue", "yellow", "magenta", "green"];
 let count = 0;
 let interval;
 
-const date = new Date();
+const effect = () => {
+  const color = colors[count++ % colors.length];
+  postMessage([new Date(), color]);
+};
 
 onmessage = (event) => {
   switch (event.data) {
@@ -10,11 +13,8 @@ onmessage = (event) => {
       if (interval) clearInterval(interval);
       break;
     case "start":
-      interval = setInterval(() => {
-        date.setSeconds(date.getSeconds() + 1);
-        const color = colors[count++ % colors.length];
-        postMessage([date, color]);
-      }, 1000);
+      effect();
+      interval = setInterval(effect, 1000);
       break;
   }
 };
